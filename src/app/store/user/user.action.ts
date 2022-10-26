2e28860930f47fa9010137f0bde3d96939f1ad5b
Muneersahel <saidmunir73@gmail.com>
2022-10-26 11:57:34 +0300

    add user actions

---
+++ src/app/store/user/user.action.ts
@@ -0,0 +1,29 @@
+import { createAction, props } from '@ngrx/store';
+import { UserInterface } from '@utils/interfaces/user.interface';
+
+class UserActionsClass {
+  getUser = createAction('[User] Get user');
+  getUserSuccess = createAction(
+    '[User] Get user success',
+    props<{ user: UserInterface }>()
+  );
+  getUserError = createAction(
+    '[User] Get user error',
+    props<{ error: string }>()
+  );
+
+  createUser = createAction(
+    '[User] Create user',
+    props<{ user: UserInterface }>()
+  );
+  createUserSuccess = createAction(
+    '[User] Create user success',
+    props<{ user: UserInterface }>()
+  );
+  createUserError = createAction(
+    '[User] Create user error',
+    props<{ error: string }>()
+  );
+}
+
+export const UserActions = new UserActionsClass();
